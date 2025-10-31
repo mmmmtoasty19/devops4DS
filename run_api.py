45561c4a7c7683d919e2ef6b422fc92bd9d60edc
Kyle <kyleb44@hotmail.com>
2025-10-31 00:13:44 -0400

    update for s3 storage

--- run_api.py
+++ run_api.py
@@ -1,11 +1,11 @@
 # If model has changed make sure to rerun/render model-py.qmd
 # run this script from the command line uv run run_api.py
 
-from pins import board_folder
+from pins import board_s3
 from vetiver import VetiverAPI, VetiverModel
 
 # Load the model from the pin
-model_board = board_folder("/data/model", allow_pickle_read=True)
+model_board = board_s3("do4ds-lab-kyle", allow_pickle_read=True)
 v = VetiverModel.from_pin(model_board, "penguin_model")
 
 # Create API
